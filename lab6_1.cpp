e1e278edeba20ad22ffb0241cd9e5205c3dfbbbb
Pongsapak <pakkadjor00@gmail.com>
2022-12-13 12:09:44 +0700

    "lab6_1.cpp completed"

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,28 @@
 #include<iostream>
+#include<string>
 using namespace std;
+int num[100],i=1,j=0,count=0,even=0,odd=0;
 
 int main(){
+
+    while(i==1){
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >>num[j];
+    if(num[j]==0){
+        break;
+    }
+    j++;
+    }
+    while(count<j){
+    if(num[count]%2==0){
+        even++;
+    }
+    else{
+        odd++;
+    }
+    count++;
+    }
+    cout << "#Even numbers = "<<even<<"\n";
+    cout << "#Odd numbers = "<<odd<<"\n";
     return 0;
 }
